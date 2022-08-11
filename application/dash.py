88b4b08a4cf3c4fe19216bff58f40440a1b19196
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2022-08-11 16:15:16 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -21,7 +21,7 @@ import sidetable as stb
 import datetime
 from datetime import datetime, timedelta
 from datetime import date
-import geopandas as gpd
+#import geopandas as gpd
 import flask
 import os
 
@@ -133,7 +133,7 @@ femi15_21 = fg[[
  'Enero21','Febrero21','Marzo21','Abril21','Mayo21','Junio21','Julio21',
 'Agosto21','Septiembre21','Octubre21','Noviembre21','Diciembre21',
     
- 'Enero22',# 'Febrero22', 'Marzo22','Abril22', 'Mayo22', 'Junio22', 'Julio22', 'Agosto22',
+ 'Enero22', 'Febrero22', 'Marzo22','Abril22', 'Mayo22', 'Junio22', #'Julio22', 'Agosto22',
    #'Septiembre22','Octubre22','Noviembre22','Diciembre22'
              ]]
 
@@ -161,8 +161,8 @@ femi15_21['Total2020']= femi15_21[[ 'Enero20', 'Febrero20', 'Marzo20', 'Abril20'
 femi15_21['Total2021']= femi15_21[[ 'Enero21','Febrero21', 'Marzo21', 'Abril21', 'Mayo21',
                                    'Junio21','Julio21','Agosto21','Septiembre21','Octubre21',
                                    'Noviembre21','Diciembre21']].sum(axis=1)
-femi15_21['Total2022']= femi15_21[[ 'Enero22', #'Febrero22', 'Marzo22', 'Abril22', 'Mayo22',
-                              # 'Junio22', 'Julio22', 'Agosto22', 'Septiembre22', 'Octubre22',
+femi15_21['Total2022']= femi15_21[[ 'Enero22', 'Febrero22', 'Marzo22', 'Abril22', 'Mayo22',
+                               'Junio22', #'Julio22', 'Agosto22', 'Septiembre22', 'Octubre22',
                                #'Noviembre22', 'Diciembre22',
                                   ]].sum(axis=1)
 
@@ -201,7 +201,7 @@ pagra = fg[[
  'Enero21', 'Febrero21', 'Marzo21','Abril21', 'Mayo21', 'Junio21', 'Julio21', 'Agosto21',
     'Septiembre21','Octubre21','Noviembre21','Diciembre21',
     
- 'Enero22',# 'Febrero22', 'Marzo22','Abril22', 'Mayo22', 'Junio22', 'Julio22', 'Agosto22',
+ 'Enero22', 'Febrero22', 'Marzo22','Abril22', 'Mayo22', 'Junio22', #'Julio22', 'Agosto22',
    #'Septiembre22','Octubre22','Noviembre22','Diciembre22'
             ]]
 
@@ -691,7 +691,7 @@ body = html.Div([
                        dbc.Badge("mensuales", color="info", className="mr-1")]), 
                                        width={'size': 11,  "offset":1 })]),
        dbc.Row([        
-               dbc.Col(html.H5("(hasta enero 2022)"),
+               dbc.Col(html.H5("(hasta junio 2022)"),
                                        width={ 'size': 3, "offset":1 }),
 
             ]),
